f99c3092815c4c7a6feaac7fda19438b25b49896
swarnendu <sonaisg@rediffmail.com>
2018-07-19 14:03:07 +0530

    Update

--- main.cpp
+++ main.cpp
@@ -11,8 +11,9 @@ Mat canny_output;
 vector<vector<Point>> contours;
 vector<Vec4i> hierarchy;
 vector<Vec4i> hierarchy_update;
-#define length = 1280
-#define width =
+#define length  1280
+#define width 720
+#define w 400
 int max_trackbar = 500;
 const char* standard_name = "Standard Hough Lines Demo";
 const char* probabilistic_name = "Probabilistic Hough Lines Demo";
@@ -28,6 +29,18 @@ bool compareContourAreas ( std::vector<cv::Point> contour1, std::vector<cv::Poin
     double j = fabs( contourArea(cv::Mat(contour2)) );
     return ( i < j );
 }
+void MyFilledCircle( Mat img, Point center )
+{
+    int thickness = -1;
+    int lineType = 8;
+
+    circle( img,
+            center,
+            w/32.0,
+            Scalar( 0, 0, 255 ),
+            thickness,
+            lineType );
+}
 void Standard_Hough( int, void* )
 {
     vector<Vec2f> s_lines;
@@ -41,16 +54,52 @@ void Standard_Hough( int, void* )
     //cout << s_lines.size()<< endl;
     for( size_t i = 0; i < s_lines.size(); i++ )
     {
-        float r = s_lines[i][0], t = s_lines[i][1];
-        cout << i << endl;
-        cout << r << " "<< t << endl;
-        double cos_t = cos(t), sin_t = sin(t);
-        double x0 = r*cos_t, y0 = r*sin_t;
-        double alpha = 1000;
+        int no_pt=0;
+        float r1 = s_lines[i][0], t1 = s_lines[i][1];
+        double c2 = cos(t1), c1 = sin(t1);
+        for( size_t j = 0; j< s_lines.size(); j++ )
+        {
+            float r2 = s_lines[j][0], t2 = s_lines[j][1];
+            double c4 = cos(t2), c3 = sin(t2);
+            if(abs(t1-t2)>20 || abs(t1-t2)>0.1 )
+            {
+                int x = cvRound((r2*c1-r1*c3)/(c1*c4-c2*c3));
+                int y = cvRound((r1-x*c2)/c1);
+                Point pt(x,y);
+                int dilation_size = 1 ;
+                Mat element = getStructuringElement( MORPH_RECT,
+                                                     Size( 2*dilation_size + 1, 2*dilation_size+1 ),
+                                                     Point( dilation_size, dilation_size ) );
+                /// Apply the dilation operation
+                dilate(standard_hough,standard_hough,element);
+                // applying dilation in an
+                int erosion_size = 1 ;
+                Mat element_e = getStructuringElement( MORPH_RECT,
+                                                       Size( 2*erosion_size+ 1, 2*erosion_size+1 ),
+                                                       Point( erosion_size,erosion_size ) );
+                /// Apply the erosion operation
+                erode(standard_hough,standard_hough,element);
+                double x01= r1*c2, y01 = r1*c1;
+                no_pt++;
+                double alpha = 1000;
+                Point pt1( cvRound(x01 + alpha*(-c1)), cvRound(y01 + alpha*c2) );
+                Point pt2( cvRound(x01 - alpha*(-c1)), cvRound(y01 - alpha*c2) );
+                line( standard_hough, pt1, pt2, Scalar(255,255,255), 3, LINE_AA);
+                double x02= r2*c4, y02 = r2*c3;
+                Point pt3( cvRound(x02 + alpha*(-c3)), cvRound(y02 + alpha*c4) );
+                Point pt4( cvRound(x02 - alpha*(-c3)), cvRound(y02 - alpha*c4) );
+                line( standard_hough, pt3, pt4, Scalar(255,255,255), 3, LINE_AA);
+                MyFilledCircle(standard_hough,pt);
+                cout << no_pt << endl;
 
-        Point pt1( cvRound(x0 + alpha*(-sin_t)), cvRound(y0 + alpha*cos_t) );
-        Point pt2( cvRound(x0 - alpha*(-sin_t)), cvRound(y0 - alpha*cos_t) );
-        line( standard_hough, pt1, pt2, Scalar(255,255,255), 3, LINE_AA);
+            }
+        }
+
+        //double x0 = r*cos_t, y0 = r*sin_t;
+//        double alpha = 1000;
+//        Point pt1( cvRound(x0 + alpha*(-sin_t)), cvRound(y0 + alpha*cos_t) );
+//        Point pt2( cvRound(x0 - alpha*(-sin_t)), cvRound(y0 - alpha*cos_t) );
+//        line( standard_hough, pt1, pt2, Scalar(255,255,255), 3, LINE_AA);
 //        pt.push_back(pt1);
 //        pt.push_back(pt2);
 
@@ -60,31 +109,12 @@ void Standard_Hough( int, void* )
 //        pt.pop_back();
 //        pt.pop_back();
     }
-    //if(!lines.empty())
-    //{
-
-    //}
-    //cout << lines[1]<< endl;
-    int dilation_size = 1 ;
-    Mat element = getStructuringElement( MORPH_RECT,
-                                         Size( 2*dilation_size + 1, 2*dilation_size+1 ),
-                                         Point( dilation_size, dilation_size ) );
-    /// Apply the dilation operation
-    dilate(standard_hough,standard_hough,element);
-    // applying dilation in an
-    int erosion_size = 1 ;
-    Mat element_e = getStructuringElement( MORPH_RECT,
-                                           Size( 2*erosion_size+ 1, 2*erosion_size+1 ),
-                                           Point( erosion_size,erosion_size ) );
-    /// Apply the erosion operation
-    erode(standard_hough,standard_hough,element);
-    /// Applying erosion and dilation for getting continuous lines
-    cout << standard_hough.size()<< endl;
+//    cout << standard_hough.size()<< endl;
     imshow( standard_name, standard_hough );
 }
 
 int main(int argc,char** argv) {
-    String imagename("/home/swarnendu/Downloads/pics/8.jpeg");
+    String imagename("/home/swarnendu/Downloads/pics/5.jpeg");
     // reading the image path
     if(argc>1)
     {
@@ -120,7 +150,11 @@ int main(int argc,char** argv) {
     // Naming the window for printing the original image
     namedWindow(source_window,WINDOW_AUTOSIZE);
     // opening the window for display
-    imshow(source_window,src);
+
+
+
+
+    imshow(source_window,src_gray);
     // displaying the image
 
     Canny(src_gray,canny_output,thresh,thresh*2,3);
